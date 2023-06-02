2147b2bec4dee8176c4f2b5aabb7560456f437e6
Jason Held <jason.held@empassion.com>
2023-06-02 14:59:27 -0400

    Fixes and some improvements to the directive

--- src/main/java/org/example/directives/SendToErrorMulti.java
+++ src/main/java/org/example/directives/SendToErrorMulti.java
@@ -4,15 +4,24 @@ import com.google.common.base.Joiner;
 import io.cdap.cdap.api.annotation.Description;
 import io.cdap.cdap.api.annotation.Name;
 import io.cdap.cdap.api.annotation.Plugin;
-import io.cdap.wrangler.api.*;
-import io.cdap.wrangler.api.annotations.Categories;
+import io.cdap.wrangler.api.Arguments;
+import io.cdap.wrangler.api.Directive;
+import io.cdap.wrangler.api.DirectiveExecutionException;
+import io.cdap.wrangler.api.DirectiveParseException;
+import io.cdap.wrangler.api.ErrorRowException;
+import io.cdap.wrangler.api.ExecutorContext;
+import io.cdap.wrangler.api.Optional;
+import io.cdap.wrangler.api.Row;
 import io.cdap.wrangler.api.parser.Text;
+import io.cdap.wrangler.api.annotations.Categories;
+import io.cdap.wrangler.api.lineage.Lineage;
+import io.cdap.wrangler.api.lineage.Mutation;
 import io.cdap.wrangler.api.parser.TokenType;
 import io.cdap.wrangler.api.parser.UsageDefinition;
-import org.example.directives.expression.EL;
-import org.example.directives.expression.ELContext;
-import org.example.directives.expression.ELException;
-import org.example.directives.expression.ELResult;
+import io.cdap.wrangler.expression.EL;
+import io.cdap.wrangler.expression.ELContext;
+import io.cdap.wrangler.expression.ELException;
+import io.cdap.wrangler.expression.ELResult;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -20,67 +29,60 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * A directive for erroring the record if
- * <p>
- * <p>
- * This step will evaluate the conditions, if the conditions evaluates to
- * true, then the row will be skipped. If the conditions evaluates to
- * false, then the row will be accepted.
- * </p>
+ * This class <code>SendToErrorMulti</code>implements a <code>Directive</code> interface
+ * for sending all respective matching errors on a given row to the error collector.
  */
 @Plugin(type = Directive.TYPE)
 @Name(SendToErrorMulti.NAME)
 @Categories(categories = {"row", "data-quality"})
-@Description("Send records that match the given conditions to the error collector.")
-public class SendToErrorMulti implements Directive {
-    static final String NAME = "send-to-error-multi";
+@Description("Send records and all respective matches of the provided conditions to the error collector.")
+public final class SendToErrorMulti implements Directive, Lineage {
+    public static final String NAME = "send-to-error-multi";
     private Map<String, ConditionInfo> conditions = new HashMap<>();
-
+    
     private static final class ConditionInfo {
         private final EL compiledCondition;
         private final String metric;
-
+	
         private ConditionInfo(EL compiledCondition, String metric) {
             this.compiledCondition = compiledCondition;
             this.metric = metric;
         }
     }
-
+    
     @Override
     public UsageDefinition define() {
         UsageDefinition.Builder builder = UsageDefinition.builder(NAME);
-        // TODO: define the conditions and metrics parameters as TokenType.TEXT. Make the metrics parameter optional.
         builder.define("conditions", TokenType.TEXT);
-        builder.define("metrics", TokenType.TEXT);
+        builder.define("metrics", TokenType.TEXT, Optional.TRUE);
         return builder.build();
 
     }
 
     @Override
     public void initialize(Arguments args) throws DirectiveParseException {
-        // TODO: read the value of the parameter "conditions" in the variable "conditionsParamValue"
-        String conditionsParamValue = null;
+        String conditionsParamValue =  ((Text) args.value("conditions")).value();
         String[] conditionsList = conditionsParamValue.split(",");
         String[] metricsList = null;
-        // TODO: read the value of the "metrics" parameter in the variable "metricsParamValue" only if the metrics param
         // exists, since it is an optional parameter
         if (args.contains("metrics")) {
-            String metricsParamValue = null;
+            String metricsParamValue = ((Text) args.value("metrics")).value();
             metricsList = metricsParamValue.split(",");
         }
         if (metricsList != null && conditionsList.length != metricsList.length) {
             throw new DirectiveParseException("Number of metrics should be equal to the number of conditions. Conditions = " +
-                    conditionsList.length + "; metrics = " + metricsList.length);
+					      conditionsList.length + "; metrics = " + metricsList.length);
         }
         // Populate the conditions map
         for (int i = 0; i < conditionsList.length; i++) {
-            EL el = new EL(new EL.DefaultFunctions());
+            EL el;
+	    
             try {
-                el.compile(conditionsList[i]);
+		el = EL.compile(conditionsList[i]);
             } catch (ELException e) {
                 throw new DirectiveParseException(
-                        String.format("Invalid conditions '%s'.", conditions)
-                );
+						  String.format("Invalid conditions '%s'.", conditions)
+						  );
             }
             String metric = metricsList != null ? metricsList[i] : null;
             conditions.put(conditionsList[i], new ConditionInfo(el, metric));
@@ -88,21 +90,14 @@ public class SendToErrorMulti implements Directive {
     }
 
     @Override
-    public void destroy() {
-        // no-op
-    }
-
-    @Override
     public List<Row> execute(List<Row> rows, ExecutorContext context)
-            throws DirectiveExecutionException, ErrorRowException {
+	throws DirectiveExecutionException, ErrorRowException {
         List<Row> results = new ArrayList<>();
         for (Row row : rows) {
             // Move the fields from the row into the context.
             ELContext ctx = new ELContext();
             ctx.set("this", row);
-            boolean atLeastOneConditionSatisfied = false;
             List<String> satisfiedConditions = new ArrayList<>();
-            // for each row, go through each condition, and book-keep conditions that are not satisfied
             for (Map.Entry<String, ConditionInfo> conditionEntry : conditions.entrySet()) {
                 String condition = conditionEntry.getKey();
                 ConditionInfo conditionInfo = conditionEntry.getValue();
@@ -123,26 +118,40 @@ public class SendToErrorMulti implements Directive {
                 // mapped into context.
                 try {
                     ELResult result = el.execute(ctx);
+				       
                     if (result.getBoolean()) {
                         if (metric != null && context != null) {
                             context.getMetrics().count(metric, 1);
                         }
-                        // TODO: since the result of executing at least one condition is true, you want to send to error
-                        // so set the atLeastOneConditionSatisfied variable to 1 in such a case
-                        // also, since we want to capture all the conditions that were met, capture the condition that was satisfied
-                        // in the satisfiedConditions list
+			satisfiedConditions.add(condition);
                     }
                 } catch (ELException e) {
                     throw new DirectiveExecutionException(e.getMessage());
                 }
             }
-            if (atLeastOneConditionSatisfied) {
-                // TODO: If at least 1 condition is satisfied, we want to send to error
-                // Do that by throwing an ErrorRowException. Set the message in the exception to be a comma separated list of
-                // all satisfied conditions
+            if (!satisfiedConditions.isEmpty()) {
+		throw new ErrorRowException(NAME, Joiner.on(",").join(satisfiedConditions), 1);
             }
             results.add(row);
         }
         return results;
+    }
+
+
+    @Override
+    public Mutation lineage() {
+	Mutation.Builder builder = Mutation.builder()
+	    .readable("Redirecting records to error path based on expression '%s'", Joiner.on(",").join(conditions.keySet().toArray()));
+	for (Map.Entry<String, ConditionInfo> conditionEntry : conditions.entrySet()) {
+	    ConditionInfo conditionInfo = conditionEntry.getValue();
+	    EL el = conditionInfo.compiledCondition;
+	    el.variables().forEach(column -> builder.relation(column, column));
+	}
+	return builder.build();
+    }
+
+    @Override
+    public void destroy() {
+	// no-op
     }
 }
--- src/test/java/org/example/directives/SendToErrorMultiTest.java
+++ src/test/java/org/example/directives/SendToErrorMultiTest.java
@@ -5,6 +5,7 @@ import io.cdap.wrangler.api.Row;
 import io.cdap.wrangler.test.TestingRig;
 import io.cdap.wrangler.test.api.TestRecipe;
 import io.cdap.wrangler.test.api.TestRows;
+import io.cdap.wrangler.api.ErrorRecord;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,26 +15,56 @@ import java.util.List;
  * Tests {@link SendToErrorMulti}
  */
 public class SendToErrorMultiTest {
+
     @Test
-    public void testBasicReverse() throws Exception {
-        TestRecipe recipe = new TestRecipe();
-        recipe.add("parse-as-csv :body ',';");
-        recipe.add("drop :body");
-        recipe.add("set-headers :encounter,:age,:first,:last;");
-        // TODO: Add the send-to-error-multi directive with two conditions:
-        // 1. checks that the encounter is empty
-        // 2. checks that age is less than or equal to 0
+    public void testErrorMultiMetric() throws Exception {
+	TestRecipe recipe = new TestRecipe();
+	recipe.add("parse-as-csv :body ',';");
+	recipe.add("drop :body");
+	recipe.add("set-headers :a,:b,:c;");
 
-        TestRows rows = new TestRows();
-        rows.add(new Row("body", "en1,0,john,doe"));
-        rows.add(new Row("body", "en2,20,samuel,jackson"));
-        rows.add(new Row("body", ",30,sam,foe"));
+	recipe.add("send-to-error-multi \"!a.isEmpty(),b=='joltie'\" \"my_metric,joltie_metric\"");
 
-        RecipePipeline pipeline = TestingRig.pipeline(SendToErrorMulti.class, recipe);
-        List<Row> actual = pipeline.execute(rows.toList());
-        List errors = pipeline.errors();
+	TestRows rows = new TestRows();
+	rows.add(new Row("body", "root,joltie,mars avenue"));
+	rows.add(new Row("body", ",root,venus blvd"));
 
-        Assert.assertEquals(1, actual.size());
-        Assert.assertEquals(2, errors.size());
+	RecipePipeline pipeline = TestingRig.pipeline(SendToErrorMulti.class, recipe);
+	List<Row> actual = pipeline.execute(rows.toList());
+	List<ErrorRecord> errors = pipeline.errors();
+
+	Assert.assertEquals(1, actual.size());
+	Assert.assertEquals(1, errors.size());
+	Assert.assertTrue(errors.get(0).getMessage().split(",")[1].startsWith("!a.isEmpty()"));
+	Assert.assertTrue(errors.get(0).getMessage().split(",")[0].startsWith("b=='joltie'"));
+
+	Assert.assertEquals("", actual.get(0).getValue("a"));
+	Assert.assertEquals("root", actual.get(0).getValue("b"));
+    }
+    @Test
+    public void testErrorMultiNoMetric() throws Exception {
+	TestRecipe recipe = new TestRecipe();
+	recipe.add("parse-as-csv :body ',';");
+	recipe.add("drop :body");
+	recipe.add("set-headers :a,:b,:c;");
+
+	recipe.add("send-to-error-multi \"!a.isEmpty(),b=='joltie'\"");
+
+	TestRows rows = new TestRows();
+	rows.add(new Row("body", "root,joltie,mars avenue"));
+	rows.add(new Row("body", ",root,venus blvd"));
+
+	RecipePipeline pipeline = TestingRig.pipeline(SendToErrorMulti.class, recipe);
+	List<Row> actual = pipeline.execute(rows.toList());
+	List<ErrorRecord> errors = pipeline.errors();
+
+	Assert.assertEquals(1, actual.size());
+	Assert.assertEquals(1, errors.size());
+	Assert.assertTrue(errors.get(0).getMessage().split(",")[1].startsWith("!a.isEmpty()"));
+	Assert.assertTrue(errors.get(0).getMessage().split(",")[0].startsWith("b=='joltie'"));
+
+	Assert.assertEquals("", actual.get(0).getValue("a"));
+	Assert.assertEquals("root", actual.get(0).getValue("b"));
+
     }
 }
